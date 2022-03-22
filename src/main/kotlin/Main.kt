9243871cef387014470edd63dc8fbf5c9d956afc
cassio.silva <cassiodavid93@gmail.com>
2022-03-22 16:46:07 -0300

    "basico sintaxe"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,83 @@
+/*
+* Estudos Kotlin basico
+* */
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val a: Int = 1  // immediate assignment
+    val b = 2       // `Int` type is inferred
+
+    val c: Int      // Type required when no initializer is provided
+    c = 3           // deferred assignment
+
+    var xp = 5 // `Int` type is inferred
+    xp += 1
+
+    val PI = 3.14
+    var x = 0
+
+    // louco demais! uahrUHRUERH
+    fun incrementX() {
+        fun dentro(y: Int): Int  {
+            return y
+        }
+
+      x = dentro(5)
+    }
+    println(x)
+
+
 }
+
+
+fun sum1(a: Int, b: Int): Int {
+    return a + b
+}
+fun sum(a: Int, b: Int) = a + b
+
+fun printSum(a: Int, b: Int): Unit {
+    println("sum of $a and $b is ${a + b}")
+}
+
+fun maxOf(a: Int, b: Int): Int {
+    if (a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+
+fun maxOf2(a: Int, b: Int) = if (a > b) a else b
+
+
+fun forItens() {
+
+    val items = listOf("apple", "banana", "kiwifruit")
+    for (item in items) {
+        println(item)
+    }
+
+
+    val items2 = listOf("apple", "banana", "kiwifruit")
+    for (index in items2.indices) {
+        println("item at $index is ${items2[index]}")
+    }
+}
+
+fun whilleLoop(){
+
+    val items = listOf("apple", "banana", "kiwifruit")
+    var index = 0
+    while (index < items.size) {
+        println("item at $index is ${items[index]}")
+        index++
+    }
+}
+//Aqui precisa ser estudado "when"
+fun describe(obj: Any): String =
+    when (obj) {
+        1          -> "One"
+        "Hello"    -> "Greeting"
+        is Long    -> "Long"
+        !is String -> "Not a string"
+        else       -> "Unknown"
+    }
